8e963c637be7966a6a7cfcc9c02bafe15216c650
SR TAMIM <86656406+sr-tamim@users.noreply.github.com>
2021-08-27 20:50:51 +0600

    Add files via upload

--- random-users-api/random-user.js
+++ random-users-api/random-user.js
@@ -1,9 +1,8 @@
 
 const loadUser = () => {
-    document.getElementById('userContainer').innerHTML = `
-        <h2 class='d-flex align-items-center justify-content-center' style='height:200px'>
-            Loading...
-        </h2>`;
+    document.getElementById('loadingMessage').style.display = 'block';
+    document.getElementById('userContainer').style.display = 'none';
+
     fetch('https://randomuser.me/api/1.3/')
         .then(Response => Response.json())
         .then(data => displayUser(data))
@@ -11,25 +10,26 @@ const loadUser = () => {
 
 const displayUser = data => {
     const user = (data.results[0]);
+    const imgContainer = document.getElementById('userImg');
+    const nameContainer = document.getElementById('userName');
+    const infoContainer = document.getElementById('userInfo');
+    const mailContainer = document.getElementById('userMail');
+    const contactContainer = document.getElementById('userContact');
+    const addressContainer = document.getElementById('userAddress');
 
-    document.getElementById('userContainer').innerHTML = `
-        <div class='bg-white p-5 shadow-lg d-inline-block' style='border-radius:10px'>
-            <div class='position-relative d-flex justify-content-center mt-4'>
-                <img src='${user.picture.large}' class='rounded-3 position-absolute bottom-0'>
-            </div>
-            <h2 class='mt-3'>
-                <span class='font-monospace'>${user.name.title}.</span> ${user.name.first + ' ' + user.name.last}
-            </h2>
-            <h5 class='mb-3 text-capitalize'>
-                Gender: ${user.gender} | Age: ${user.dob.age}
-            </h5>
-            <h6>${user.email}</h6>
-            <h6>Contact: ${user.cell.toString()}</h6>
-            <small class='d-inline-block mt-2'>
-                <span class='fs-6 text-decoration-underline'>Address</span><br>
-                ${user.location.street.number} ${user.location.street.name}, ${user.location.city},<br>${user.location.state}, ${user.location.country}
-            </small>
-        </div>`
+    imgContainer.setAttribute('src', user.picture.large);
+    nameContainer.innerHTML = `
+        <span class='font-monospace'>${user.name.title}.</span> ${user.name.first + ' ' + user.name.last}`;
+    infoContainer.innerHTML = `
+        Gender: ${user.gender} | Age: ${user.dob.age}`;
+    mailContainer.innerHTML = `${user.email}`;
+    contactContainer.innerHTML = `Contact: ${user.cell.toString()}`;
+    addressContainer.innerHTML = `
+        <span class='fs-6 text-decoration-underline'>Address</span><br>
+        ${user.location.street.number} ${user.location.street.name}, ${user.location.city},<br>${user.location.state}, ${user.location.country}`;
+
+    document.getElementById('loadingMessage').style.display = 'none';
+    document.getElementById('userContainer').style.display = 'inline-block';
     console.log(user);
 }
 
